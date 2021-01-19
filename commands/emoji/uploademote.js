8f94fa5102195e5f3bdabc98a1dcf3ae8ce318ee
Kasakshi <77403361+Kasakshi-Hatake@users.noreply.github.com>
2021-01-19 13:46:04 +0530

    Update uploademote.js

--- commands/emoji/uploademote.js
+++ commands/emoji/uploademote.js
@@ -1,3 +1,6 @@
+const { parse } = require("twemoji-parser");
+const Discord = require('discord.js');
+
 module.exports = {
     config: {
         name: 'upload_emote',
@@ -7,24 +10,46 @@ module.exports = {
         accessableby: "MANAGE_EMOJIS",
     },
     run: async (client, message, args) => {
-    if(!args[0]) return message.channel.send('Please give me the name of the emoji')
-    if(!args[1]) return message.channel.send('Please give me a emoji or attachment or link')
-  function isURL(url) {
-  if (!url) return false;
-   var pattern = new RegExp("/(https?:\/\/[^\s]+\.(?:png|jpg|jpeg|bmp|gif|tiff|webp)(?:$|[^\s]+))/i")
-   return pattern.test(url)
-   }
-   if(message.content.includes("https://") {
-   if(!isUR(args[1]) return;
-   message.guild.emojis.create(`${args[1]}`, `${args[0]}`).then(emoji => message.channel.send(`Created a emoji named ${emoji}`)
-   }
-   function isEmoji(emoji) {
-   if(!emoji) return false;
-   var pattern = new RegExp("<a:.+?:\d+>|<:.+?:\d+>")
-   return pattern.test(emoji)
-   }
-if(isEmoji(args[1])) {
-message.guild.create(args[1], args[0]).then(emoji => message.channel.send(`Created a emoji named ${emoji}`)
-}
 
+        if (!message.member.hasPermission(`MANAGE_EMOJIS`)) {
+            return message.channel.send(`:warning: You don't have the permissions to use this command `)
+        }
+           const emoji = args[0];
+        if (!emoji) return message.channel.send(`Please Give Me A Emoji!`);
+
+        let customemoji = Discord.Util.parseEmoji(emoji);
+        if (customemoji.id) {
+            const Link = `https://cdn.discordapp.com/emojis/${customemoji.id}.${customemoji.animated ? "gif" : "png"
+                }`;
+            const name = args.slice(1).join(" ");
+            
+        
+ try {
+                const Added = new MessageEmbed()
+                .setColor("RANDOM")
+                .setTitle(`:white_Check_mark: Emoji Added`)
+                .setDescription(
+                `:white_check_mark: Emoji Has Been Added! | Name : ${name || `${customemoji.name}`} | Preview : [Click Me](${Link})`
+                    );
+                await message.guild.emojis.create(
+                    `${Link}`,
+                    `${name || `${customemoji.name}`}`
+                )
+                return message.channel.send(Added)
+            } catch (err) {
+                console.log(err)
+                return message.channel.send(`${client.emotes.error} An error has occured!\n\n**Possible Reasons:**\n\`\`\`- This server has reached the emojis limit\n- The bot doesn't have permissions.\n- The emojis size is too big.\`\`\``)
+           
+            } 
+             } else {
+                  let CheckEmoji = parse(emoji, { assetType: "png" });
+            if (!CheckEmoji[0])
+                return message.channel.send(`**Please Give Me A Valid Emoji!**`);
+            message.channel.send(
+                ` **You Can Use Normal Emoji Without Adding In Server!**`
+            );
+        }
+
+    }
+}
    
