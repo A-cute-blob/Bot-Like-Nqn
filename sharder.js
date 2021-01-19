c78f6f39bf09a19331bee9cf470525fa73efcae1
Kasakshi <77403361+Kasakshi-Hatake@users.noreply.github.com>
2021-01-19 22:38:10 +0530

    Update sharder.js

--- sharder.js
+++ sharder.js
@@ -2,7 +2,7 @@ const { ShardingManager } = require('discord.js');
 const { token } = require('./config.js')
 const Token = token;
 
-const manager = new ShardingManager('./server.js', { token: Token, autoSpawn: true, respawn: true }); 
+const manager = new ShardingManager('./emoji.js', { token: Token, autoSpawn: true, respawn: true }); 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 
 manager.spawn();
