f80da41305aecb487127313f8336ac6052328f74
Kasakshi <77403361+Kasakshi-Hatake@users.noreply.github.com>
2021-01-19 14:10:07 +0530

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,17 +1,19 @@
 const config = require('../config.js');
 const Discord = require('discord.js');
-const db = require('quick.db');
+const db = require('wio.db');
 const chalk = require('chalk');
 
 module.exports = async (client, message) => {
     let prefix = config.prefix
+    let Prefix = db.fetch(`Prefix_${message.guild.id}`)
+    if(!Prefix) Prefix = prefix;
     try{
         if (message.author.bot || message.channel.type === "dm") return;
         if(!message.content.startsWith(prefix)) return;
     
      
     
-        let args = message.content.slice(prefix.length).trim().split(/ +/g);
+        let args = message.content.slice(Prefix.length).trim().split(/ +/g);
         let cmd = args.shift().toLowerCase();
 
         var commandfile = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd))
